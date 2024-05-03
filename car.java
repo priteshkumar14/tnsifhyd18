12300067807c5b7d39e37f812e572e1ed1f923a1
priteshkumar14 <168433453+priteshkumar14@users.noreply.github.com>
2024-05-03 14:42:27 +0530

    Add files via upload
    gettershetter

--- car.java
+++ car.java
@@ -1,10 +1,36 @@
-package com.oop1.constructor;
+package gettersetter;
 
 public class car {
-	
-	public car() {
-		
-		//Code block constructor general
+	private String name;
+	private int speed;
+	private String colour;
+	private String engine;
+	public String getName() {
+		return name;
 	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public String getEngine() {
+		return engine;
+	}
+	public void setEngine(String engine) {
+		this.engine = engine;
+	}
+	
+	
+	
 
 }
---
+++ main.java
@@ -0,0 +1,21 @@
+package gettersetter;
+
+public class main {
+
+	public static void main(String[] args) {
+		car obj=new car();
+		obj.setName("innova");
+		System.out.println(obj.getName());
+		obj.setSpeed(40);
+		System.out.println(obj.getSpeed());
+		obj.setColour("red");
+		System.out.println(obj.getColour());
+		obj.setEngine("ON");
+		System.out.println(obj.getEngine());
+		
+		
+	// TODO Auto-generated method stub
+
+	}
+
+}
